5d98a54e8542fd0fc5408cb663e8cd3738dad080
lesayobruce <lesayobruce419@gmail.com>
2023-02-09 23:14:59 +0300

    completed test

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  end
+  
+  fido = Dog.new
+  fido.bark #=> "Woof!"
+
+  class Dog
+    def bark
+      puts "Woof!"
+    end
+  end
+  
+  fido = Dog.new
+  fido.bark #=> "Woof!"
+  
+  snoopy = Dog.new
+  snoopy.bark #=> "Woof!"
+
+  class Do 
+    def sit
+        puts "The Dog is sitting"
+    end 
+
+    fido = Dog.new
+    fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class = Person
+end
+
+
+jefe = Person.new
+
+    def talk
+        puts "Hello World!"
+    end
+end
+
+jefe = Person.new
+
+def walk
+    puts "The Person is walking"
+end
+end
+
+jefe = person.new
